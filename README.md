e3ba24314b3958046fdf2699765d38f6f9609415
izzymg <izzymhg@gmail.com>
2021-05-05 11:14:08 +1200

    Add popup, styling & draft script detection

---
+++ README.md
@@ -0,0 +1,9 @@
+# Draft detector
+
+#### For The Career Academy's Totara Learn LMS
+
+* HTML Popup to be added to Totara's additional HTML section
+
+* JS script which detects if the user is currently in an unedited assessment submission window displays the popup
+
+* CSS for adding to the Totara theme's additonal styling, for styling the popup (essential as it hides the popup with display: none by default)
---
+++ index.js
@@ -0,0 +1,37 @@
+/*
+Draft detector
+JS for Totara Learn, detects if the user is currently in an unedited assessment submission window and warns them of such.
+
+Izzy MG, TCA LMS Admin
+*/
+
+(function() {
+    /**
+     * Returns true if current page has a draft submission on it.
+    */
+    function isDraftSubmission() {
+        return document.querySelector(".submissionstatusdraft") ? true : false;
+    }
+
+    /**
+     * Removes hidden attribute from the popup element placed in the page through Totara's extra HTML.
+    */
+    function showPopup() {
+        const popup = document.querySelector(".tca-draft-popup");
+        if(!popup) {
+            throw "TCA Draft Popup element not found in page";
+        }
+
+        popup.style.hidden = false;
+    }
+
+    if(!isDraftSubmission()) {
+        return;
+    }
+
+    try {
+        showPopup();
+    } catch(err) {
+        console.error(err);
+    }
+})();
